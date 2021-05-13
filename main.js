672c4bc912d05b414da90e0531338b17eab3ee08
md manzer alam <tooshort9541@gmail.com>
2021-05-13 16:50:00 +0530

    fixed the background

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var request = new XMLHttpRequest();
 function getInput() {
     var inputHandle = document.getElementById('user-handle').value;
     console.log(inputHandle);
+    request.open('GET', 'https://codeforces.com/api/user.info?handles=' + inputHandle + ';');
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        var ans = document.getElementById('answer');
+        console.log(data);
+        ans.innerHTML = JSON.stringify(data);
+    };
+    request.send();
 }
